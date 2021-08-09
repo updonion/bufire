8d1286ad644a43e07978f9aa67f0f284ab8fdccd
updonion <87442471+updonion@users.noreply.github.com>
2021-08-09 03:46:02 +0300

    Calculate md5 sum
    Also script can check if the file name contains pattern like \[[0-9a-f]{32}\]- and skip calculating sum, but I have problems with it.

--- renamer.sh
+++ renamer.sh
@@ -2,16 +2,14 @@
 
 read -p "Folder with text files: " folder
 cd "$folder"
-sum=""
 
 for file in *.txt
 do
-# echo "$file"
-# echo ${#file}
-sum=$(sha256sum "$file")
-# echo ${sum:0:10}
-newfile="[${sum:0:10}]-$file"
-if [[ "$file" == *"[${sum:0:10}]-"* ]]; then
+
+sum=$(md5sum "$file")
+newfile="[${sum:0:32}]-$file"
+
+if [[ "$file" == *"[${sum:0:32}]-"* ]]; then
   echo -e "\e[1;31mFile $file exists\e[0m"
 else
   echo -e "\e[1;34mCreated $newfile\e[0m"
